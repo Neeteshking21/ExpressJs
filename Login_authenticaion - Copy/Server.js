ca9abb5bff1985847552e20272e79267e02372bd
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-07 15:04:53 +0530

    Set validation in From and Create alert box

--- Login_authenticaion - Copy/Server.js
+++ Login_authenticaion - Copy/Server.js
@@ -21,27 +21,35 @@ app.use(express.json())
 
 app.post('/api/register', async (req, res) => {
     
-    console.log(req.body)
-    const {email, pass} = req.body
-    const encryptPass = await bcrypt.hash(pass, 10)
+    if(!validator.isEmail(req.body.email))
+         return res.json({status: 'error', error: 'Please Enter Valid Email'})
+    else if(!validator.isStrongPassword( req.body.pass ,{
+            minLength: 8, minLowercase: 1, minUppercase: 1, 
+            minNumbers: 1, minSymbols: 1, returnScore: false }
+        ))
+        return res.json({status: 'error', error: 'Password must contain 1   Lowercase, 1 Uppercase 1 number, 1 symbol and minimum length is 8'});
 
-    try{
-        const response = await User.create({
-            email:email,
-            password:encryptPass
-        })
-        console.log(`User created successfully ${response}`)
-    }
-    catch(error){
-        console.log(error.code)
-        if(error.code){
-            console.log("Email already in use")
-            return  res.json({status: 1100, error: 'Email already in use'});
+    else{
+        const {email, pass} = req.body
+        const encryptPass = await bcrypt.hash(pass, 10)
+
+            try{
+                 const response = await User.create({
+                email:email,
+                password:encryptPass
+              })
+            console.log(`User created successfully ${response}`)
         }
+        catch(error){
+            console.log(error.code)
+            if(error.code){
+              console.log("Email already in use")
+               return  res.json({status: 1100, error: 'Email already in use'});
+            }
+        }
+        res.json({status: 'ok'});
     }
-
-    console.log(encryptPass)
-    res.json({status: 'ok'});
+    
     // Analysts
     // Scripts for reading databases
     //OPEN API access to your database
