d7d8c6f04786a6cb2193ebcc62717f7ad7ca3c18
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-07 23:43:53 +0530

    JWT usage to generate token'

--- Login_authenticaion - Copy/Server.js
+++ Login_authenticaion - Copy/Server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const validator = require('validator')
 const User = require('./models/user.js')
-const async = require('hbs/lib/async')
+const jwt = require('jsonwebtoken')
 
-
+const JWT_SECRET = 'ldhhfhuagfuO3b254b784kln6l4blb63h4bil%&*^^!fjso09vhiby8ybq2vc&#Pd#*&#@^&&*ufwi8fioshiohb9yybc9vyv8y90yh0yp'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/login-app-db', {
 const app = express()
 
 app.use('/', express.static(path.join(__dirname, 'static')))
+app.use('/login', express.static(path.join(__dirname, 'static/Login.html')))
 app.use(express.json())
 
 app.post('/api/register', async (req, res) => {
@@ -63,6 +64,36 @@ app.post('/api/register', async (req, res) => {
 
      
 });
+
+app.post('/api/login', async (req, res)=>{
+
+    const { email, pass } = req.body
+    
+    if(!validator.isEmail(email))
+       return res.json({status:'error', error:'Enter Valid Email'})
+    else if(pass.length <=0)
+       return res.json({status:'error', error:'Enter password'})
+    
+    else{
+        const user = await User.findOne({email}).lean()
+        console.log(user)
+        if(user==null){
+            console.log('am i Here')
+            return res.json({status:'error', data:'Invalid Username or password'})
+           
+        }
+        console.log(user.password)
+        if( await bcrypt.compare(pass, user.password)){
+            const token = jwt.sign({
+                id: user._id,
+                email: user.email
+            }, JWT_SECRET)
+            return res.json({status:'ok', data:token})
+        }
+
+        return res.json({status: 'error', data:'Invalid Username or password'})  
+    }   
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
